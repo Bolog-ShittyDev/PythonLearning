9e3bfe7bfdc4d28b97f26916d4a77de2bcc7e78c
Bolog-Shitty <bologchristian@gmail.com>
2023-07-12 17:14:04 +0300

    changes to moon facts

--- moon_facts!.py
+++ moon_facts!.py
@@ -1,1 +1,8 @@
 print("Moon" in "This text will describe facts about the Moon")
+
+temperatures = """Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius."""
+print(temperatures.find("Mars"))
+
+temperatures = """Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius."""
+print("Mars occures " + str(temperatures.count("Mars")) + " times")
+print("Moon occures " + str(temperatures.count("Moon")) + " times")

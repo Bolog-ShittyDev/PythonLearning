d1f596271f32df65fd4b3873e426577a3aa12594
Bolog-Shitty <bologchristian@gmail.com>
2023-07-13 11:54:43 +0300

    Real Number of Planets has changed, fuck you Nasa

--- space_facts.py
+++ space_facts.py
@@ -1,24 +1,8 @@
-print("Moon" in "This text will describe facts about the Moon")
-
-temperatures = """Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius."""
-print(temperatures.find("Mars"))
-
-temperatures = """Saturn has a daytime temperature of -170 degrees Celsius, while Mars has -28 Celsius."""
-print("Mars occures " + str(temperatures.count("Mars")) + " times")
-print("Moon occures " + str(temperatures.count("Moon")) + " times")
-
-
-# case insensitive uper case and lower case:
-print("The Moon And The Earth".lower())
-print("The Moon And The Earth".upper())
-#
-
-temperatures = "Mars Average Temperature: -60 C"
-parts = temperatures.split(':')
-print(parts)
-print(parts[-1])
-
-mars_temperature = "The highest temperature on Mars is about 30 C"
-for item in mars_temperature.split():
-    if item.isnumeric():
-        print(item)
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+print(planets)
+number_of_planets = len(planets)
+print("There are currently:",number_of_planets,"Planets in our Solar System. What was that about Yugoth?")
+planets.append("Yugoth/Pluto")
+real_number_of_planets = len(planets)
+print("The government doesn't want you to know that there are really",real_number_of_planets,"planets in our solar system")
+print(planets[-1],"is the last Planet")

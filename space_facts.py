10bc1b71c39338ed331a5a7a81ddcea806e38137
Bolog-Shitty <bologchristian@gmail.com>
2023-07-14 10:42:39 +0300

    some changes i guess

--- space_facts.py
+++ space_facts.py
@@ -1,8 +1,25 @@
-planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
-print(planets)
-number_of_planets = len(planets)
-print("There are currently:",number_of_planets,"Planets in our Solar System. What was that about Yugoth?")
-planets.append("Yugoth/Pluto")
-real_number_of_planets = len(planets)
-print("The government doesn't want you to know that there are really",real_number_of_planets,"planets in our solar system")
-print(planets[-1],"is the last Planet")
+planet_moons = {
+    'mercury': 0,
+    'venus': 0,
+    'earth': 1,
+    'mars': 2,
+    'jupiter': 79,
+    'saturn': 82,
+    'uranus': 27,
+    'neptune': 14,
+    'pluto': 5,
+    'haumea': 2,
+    'makemake': 1,
+    'eris': 1
+}
+
+moons = planet_moons.values()
+total_planets = len(planet_moons.keys())
+
+total_moons = 0
+for moon in moons:
+    total_moons = total_moons + moon
+
+average = total_moons / total_planets
+
+print(f'Each planet has an average of {average} moons')
